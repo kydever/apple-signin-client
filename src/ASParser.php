a3b41da687c6f54832bf39f90e35b30e663959d5
李铭昕 <715557344@qq.com>
2022-02-22 16:32:56 +0800

    Added ASParser

---
+++ src/ASParser.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace KY\ASClient;
+
+class ASParser
+{
+    public function __construct(private ASClient $client)
+    {
+    }
+
+    public function decode(): array
+    {
+        
+    }
+}
