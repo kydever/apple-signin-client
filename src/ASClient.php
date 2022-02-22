94c0f98be7cfb49a056d557692c7653b2a39ab32
李铭昕 <715557344@qq.com>
2022-02-22 16:08:24 +0800

    增加读取公共key的接口 (#1)

--- src/ASClient.php
+++ src/ASClient.php
@@ -11,6 +11,17 @@ declare(strict_types=1);
  */
 namespace KY\ASClient;
 
+use GuzzleHttp\Client;
+
 class ASClient
 {
+    public function keys(): array
+    {
+        $client = new Client([
+            'base_uri' => 'https://appleid.apple.com/',
+        ]);
+
+        $body = (string) $client->get('auth/keys')->getBody();
+        return json_decode($body, true, flags: JSON_THROW_ON_ERROR);
+    }
 }
